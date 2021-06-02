91c66680c8b6f48a06bbf9348f3eab7519bb1b95
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-06-02 11:55:59 +0700

    adding user path box

---
+++ apps/user_path_box.py
@@ -0,0 +1,76 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_admin_components as dac
+import dash_bootstrap_components as dbc
+from example_plots import (plot_store_type_sales, plot_application_type_sales, plot_order_status)
+from IPython.core.display import HTML
+HTML("""
+<style>
+g.pointtext {display: none;}
+</style>
+""")
+
+image_filename = '/home/server/gli-data-science/akhiyar/out_plot/path_end_uninstall.png' # replace with your own image
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+
+user_path_tab = dac.TabItem(id='content_user_path', 
+                              
+    children=html.Div([
+            dbc.Row([
+              dbc.Col(
+                dbc.Card(
+                  [
+                      dbc.CardHeader(html.H5("User path (ends with uninstall apk)")),
+                      dbc.CardBody(
+                          [
+                              # html.H5("Card title", className="card-title"),
+                              html.P(
+                                    # dcc.Graph(
+                                    #   figure=plot_store_type_sales(),
+                                    #   config=dict(displayModeBar=False),
+                       
+                                    #   ),className="card-text",
+                                    html.Img(src='data:image/png;base64,{}'.format(encoded_image))
+                              ),
+                          ]),
+                  ]) ,md=12),
+              ]),
+            # dbc.Row([
+            #   dbc.Col(
+            #     dbc.Card(
+            #       [
+            #           dbc.CardHeader(html.H5("Alfagift vs WebOrder(Whatsapp) Sales")),
+            #           dbc.CardBody(
+            #               [
+            #                   # html.H5("Card title", className="card-title"),
+            #                   html.P(
+            #                         dcc.Graph(
+            #                           figure=plot_application_type_sales(),
+            #                           config=dict(displayModeBar=False),
+                       
+            #                           ),className="card-text",
+            #                   ),
+            #               ]),
+            #       ]) ,md=12),
+            #   ]),
+            # dbc.Row([
+            #   dbc.Col(
+            #     dbc.Card(
+            #       [
+            #           dbc.CardHeader(html.H5("Alfagift Order Status")),
+            #           dbc.CardBody(
+            #               [
+            #                   # html.H5("Card title", className="card-title"),
+            #                   html.P(
+            #                         dcc.Graph(
+            #                           figure=plot_order_status(),
+            #                           config=dict(displayModeBar=False),
+                       
+            #                           ),className="card-text",
+            #                   ),
+            #               ]),
+            #       ]) ,md=12),
+            #   ]),
+
+       ])
+)
