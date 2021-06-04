c52663c11f251c247e52287f06e5ab0b804b39ea
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-06-04 14:16:14 +0700

    plot general monitor in moengage

--- loader/user_path_load.py
+++ loader/user_path_load.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 
 def get_app_update():
-	app_update = pd.read_csv('/home/server/gli-data-science/akhiyar/out_plot/general_moengage/\
-		app_update.csv')
+	app_update = pd.read_csv('/home/server/gli-data-science/akhiyar/out_plot/general_moengage/app_update.csv')
 
 	return app_update
 
