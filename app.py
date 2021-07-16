a69d429c201815e8f95190bb94a50a79d3fd0d07
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-07-17 00:53:30 +0700

    change

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ sidebar = dac.Sidebar(
             dac.SidebarMenuItem(label='Soon ...', icon='cubes', children=subitems1),
 		]
 	),
-    style={'font_size':'20'},
+    style={'font-size':'20px'},
     title='Data Science Dashboard',
 	skin="light",
     color="secondary",
