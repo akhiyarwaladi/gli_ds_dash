4115837fb512681c482b403d6de49364af018b4a
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-07-17 01:52:28 +0700

    change

--- app.py
+++ app.py
@@ -125,9 +125,10 @@ right_ui = dac.NavbarDropdown(
     # ]
 )
                               
-navbar = dac.Navbar(color = "white", 
-                    text="Please navigate to one of the sidebar on the left", 
-                    children=right_ui)
+navbar = dac.Navbar(color="white", 
+                    text="select menu on left", 
+                    children=right_ui,
+                    style={'font-size':'12px','font-family':'roboto'})
 
 # Sidebar
 subitems1 = [dac.SidebarMenuSubItem(id='tab_gallery_1', 
