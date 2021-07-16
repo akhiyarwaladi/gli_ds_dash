20dbceccdde4cd899fc0782d06bcf519d6407ebf
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-07-16 22:13:52 +0700

    change sales plot

--- apps/sales.py
+++ apps/sales.py
@@ -77,7 +77,7 @@ sales_tab = dac.TabItem(id='content_sales',
                           dbc.Card(
                             dbc.CardBody(
                                 dbc.Row([
-                                    dbc.Col(html.H5("Actual sales cumulative"), width=3),
+                                    dbc.Col(html.H5("Actual sales <br> cumulative"), width=3),
                                     dbc.Col(
                                             dcc.DatePickerRange(
                                                 id='actual_sales_daterange',
