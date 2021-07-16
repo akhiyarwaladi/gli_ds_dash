fc0b774975ffc9d2681057467e64b82a1ba68de8
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-07-16 23:47:48 +0700

    change sales plot

--- apps/general_monitor.py
+++ apps/general_monitor.py
@@ -36,7 +36,7 @@ general_monitor_tab = dac.TabItem(id='content_general_monitor',
               dbc.Col(
                 dbc.Card(
                   [
-                      dbc.CardHeader(html.H5(['Alfagift' html.B(html.I('vs')). 'WebOrder(Whatsapp) Sales'])),
+                      dbc.CardHeader(html.H5(['Alfagift', html.B(html.I('vs')), 'WebOrder(Whatsapp) Sales'])),
                       dbc.CardBody(
                           [
                               # html.H5("Card title", className="card-title"),
