236cc43cdd5052019c19b037fcb7ab36fe078751
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-08-02 01:14:06 +0700

    change label

--- apps/user_path_box.py
+++ apps/user_path_box.py
@@ -80,7 +80,7 @@ user_path_tab = dac.TabItem(id='content_user_path',
                       [
                         dbc.Row([
                           dbc.Col(
-                            html.H5(['All Member', html.B(' Review '), html.B('<3 '), 'Star', html.Br(),
+                            html.H5(['All Member', html.B(' Review '), html.B('<4 '), 'Star', html.Br(),
                             "showing rows", html.Div(id='datatable_low_review_container')]), md=4),
                           dbc.Col(html.H5(""), md=2),
                           dbc.Col(
