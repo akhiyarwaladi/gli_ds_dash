a90fdd72471d0d123effd05e02bdee25a799e348
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-10-29 17:54:30 +0700

    gas gan

--- apps/promo_simulation.py
+++ apps/promo_simulation.py
@@ -94,7 +94,7 @@ promo_simulation_tab = dac.TabItem(id='content_promo_simulation',
                                         width=4,
                                     ),
 
-                                ])
+                                ]),
                                 dbc.Row([
                                     dbc.Col(
                                         dbc.FormGroup(
