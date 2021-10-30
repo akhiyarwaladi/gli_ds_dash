e607c8cee8e33a9eb5edd520b406f2436f239f77
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-10-30 17:29:49 +0700

    hadeh

--- loader/promo_simulation_load.py
+++ loader/promo_simulation_load.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 
-def get_plu_list:
+def get_plu_list():
 
 	low_label = pd.read_csv('/home/server/gli-data-science/akhiyar/sales_prediction/model/plu_list.csv'
 		,  names=['value','label'])
