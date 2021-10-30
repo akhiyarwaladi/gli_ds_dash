31d2ca532f99f3b68dbef2f585a1edf3943ae795
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-10-30 11:10:07 +0700

    gaslah

--- apps/promo_simulation.py
+++ apps/promo_simulation.py
@@ -50,8 +50,7 @@ promo_simulation_tab = dac.TabItem(id='content_promo_simulation',
                                                     max_date_allowed=dt(2022, 9, 19),
                                                     initial_visible_month=dt(2021, 8, 24),
                                                     display_format='DD-MM-Y',
-                                                    date=dt(2021, 8, 24),
-                                                    style={"width": "500px"}
+                                                    date=dt(2021, 8, 24)
                                                 ),
                                             ]
                                         ),
