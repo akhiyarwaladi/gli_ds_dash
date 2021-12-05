e16daa222c257ba9485a2406bd92e04055accf7f
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-12-05 22:43:14 +0700

    adding new model

--- app.py
+++ app.py
@@ -971,7 +971,7 @@ def show_hide_element(dropdown_promo_type_val):
 @app.callback(
     
     [
-        Output(component_id='outval_promo_simulation', component_property='children')
+        Output(component_id='outval_promo_simulation', component_property='children'),
         Output(component_id='increase_sales_adder', component_property='style'),
         Output(component_id='decrease_sales_adder', component_property='style')
     ],
