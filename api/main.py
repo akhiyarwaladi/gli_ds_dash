e7bc3d73e0a4bf343e05f9566117071cb9dbb0a4
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2022-03-16 11:17:30 +0700

    adding app main

--- api/main.py
+++ api/main.py
@@ -11,11 +11,24 @@ def predict_result(img):
 def infer_image():
     # Catch the image file from a POST request
 
-    discount_amount = request.args.get('discount_amount', None)
+    
 
-
+    promo_start_date = request.args.get('promo_start_date', None)
+    promo_end_date = request.args.get('promo_end_date', None)
+    input_min_amount = request.args.get('input_min_amount', None)
+    input_min_qty = request.args.get('input_min_qty', None)
+    input_extra_star = request.args.get('input_extra_star', None)
+    input_extra_point = request.args.get('input_extra_point', None)
+    input_discount_amount = request.args.get('input_discount_amount', None)
+    input_num_target = request.args.get('input_num_target', None)
+    pred_promo_type = request.args.get('pred_promo_type', None)
+    pred_plu = request.args.get('pred_plu', None)
+    pred_app = request.args.get('pred_app', None)
     # Return on a JSON format
 
+    print(promo_start_date)
+    print(promo_end_date)
+    print(input_min_amount)
 
     res = {
     	'sales':1000,
